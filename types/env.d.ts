97001306b693de6aa735548da5ed0531d4ede7e4
cyw <cyw19931210@163.com>
2022-10-08 14:50:26 +0800

    chore: remove shims.d.ts file

--- types/env.d.ts
+++ types/env.d.ts
@@ -1,6 +1,11 @@
 /// <reference types="vite/client" />
+declare module '*.vue' {
+  import { type DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
 
-interface ImportMetaEnv {
+interface ImportMetaEnv extends Readonly<Record<string, string>> {
   // 更多环境变量
   readonly VITE_API_HOST: string
   readonly VITE_BASE_URL: string
