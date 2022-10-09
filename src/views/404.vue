1f9ccf8438cdb7d1078021a338f6d91eca929add
cyw <cyw19931210@163.com>
2022-10-09 21:22:59 +0800

    style: 404 ui

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="not-found-page">
-    <h1 class="title">404 Not Found</h1>
-    <p>
-      <FButton v-if="showBackBtn" type="text" @click="backto"
-        >返回上一页</FButton
-      >
-    </p>
-  </div>
+  <header class="flex h-screen flex-col items-center justify-center">
+    <h1
+      class="mb-4 text-3xl font-bold leading-tight text-gray-900 dark:text-gray-100"
+    >
+      404 Page not Found
+    </h1>
+    <FButton v-if="showBackBtn" type="primary" @click="backto"
+      >返回上一页</FButton
+    >
+  </header>
 </template>
 
 <script setup lang="ts">
@@ -20,13 +22,4 @@ const backto = () => {
 }
 </script>
 
-<style>
-.not-found-page {
-  text-align: center;
-}
-.title {
-  margin-top: 220px;
-  margin-bottom: 16px;
-  font-size: 24px;
-}
-</style>
+<style></style>
